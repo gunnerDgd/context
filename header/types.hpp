734a287a0b53e08bbaf300521c8a686ead674dcc
Guest <guest-rgk54c@h11396.sgb.skir.kr>
2021-09-05 16:21:29 +0900

    Initial

---
+++ header/types.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include <iostream>
+#include <tuple>
+
+#define BRANCH_PRESERVE __attribute__((always_inline))
+
+namespace branch  {
+namespace context {
+    
+    class alignas(16) cpu_register // 64 Byte.
+    {
+    public:
+        union
+        {
+            struct
+            {
+                uint64_t     rax, rbx, rcx, rdx,
+                             rsi, rdi, rip     ; // 56 Byte
+            };  uint64_t     register_block[7] ;
+        };
+    };
+
+    class alignas(16) frame // 16 Byte
+    {
+    public:
+        union
+        {
+            struct { uint64_t rbp, rsp; }      ;
+            uint64_t          stack_register[2];
+        };
+    };
+
+    struct context_entity
+    {
+        cpu_register cpu_context  ; // 64
+        frame        stack_context; // 16
+    };
+
+    enum  execution_state
+    {
+        standby,
+        running,
+        stopped
+    };
+
+    template <typename Fp, typename... Args>
+    struct execution_argument
+    {
+        execution_argument(Fp f, Args&&... a)
+            : exec_argument(std::forward_as_tuple(a...)),
+              exec_function(f)                          {  }
+
+        Fp                  exec_function;
+        std::tuple<Args...> exec_argument;
+    };
+}
+}

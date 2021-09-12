d9216a23a2c4c8fac9af1b802190eabad17d62f9
Guest <guest-xeouub@h11468.sgb.skir.kr>
2021-09-12 16:58:39 +0900

    TLS Storage Updated

--- test.hpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-namespace test_ns {
-    thread_local extern int a;
-}
-
-void test_a();
--- test1.cpp
+++
@@ -1,7 +0,0 @@
-#include "test.hpp"
-
-int main()
-{
-    test_ns::a = 3;
-    test_a();
-}
